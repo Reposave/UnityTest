4fb074b838d25e5a5228f3e0e25ff31ca0f42ca6
Ardo Dlamini <dlmard001@cs.uct.ac.za>
2022-02-02 23:21:07 +0200

    Tried to implement Vehicle Physics.

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {   
-    public float Maxspeed = 60.0f;
+    public float Maxspeed = 6000.0f;
     float CurrentSpeed=0f;
 
     public bool PlayerOcc = false; //Tracks whether player has occupied this vehicle.
     public Rigidbody2D rb;
-    public float linearDrag = 2f;
+    public Rigidbody2D wheel;
+    public GameObject arrow;
+    public float linearDrag = 10f;
     public float mass = 0.1f;
+
+    //Quick Dirty Test...
+    public float reversing = 0; //Reversing can have two values, 0 or 180 only! This is used to flip the force directions.
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        foreach (Transform child in transform){
+                if (child.name == "Engine"){
+                    //child.GetComponent<SpriteRenderer>().enabled = false;
+                    rb = child.GetComponent<Rigidbody2D>();
+                }else if (child.name == "Wheels"){
+                    wheel = child.GetComponent<Rigidbody2D>();
+                }
+            }
+        //rb = GetComponent<Rigidbody2D>();
         rb.mass = mass;
         rb.drag = linearDrag;
-        rb.freezeRotation = true;
+        //rb.freezeRotation = true;
     }
 
     // Update is called once per frame
@@ -27,31 +40,56 @@ public class Vehicle : MonoBehaviour
         if(PlayerOcc){
             ButtonPressed();
         }
+        arrow.transform.eulerAngles = gameObject.transform.eulerAngles;
     }
-    void Move(Vector2 direction){
+    void Move(Vector2 direction,Rigidbody2D rbx,float Cspeed){
 
-        CurrentSpeed= Maxspeed;
-
-        rb.AddForce((Vector3)direction*CurrentSpeed*Time.deltaTime);
+        rbx.AddForce((Vector3)direction*Cspeed*Time.deltaTime);
         //transform.localPosition+=(Vector3)direction*CurrentSpeed*Time.deltaTime;
     }
     void ButtonPressed(){
+
+        if (Input.GetKey(KeyCode.DownArrow)){
+            Vector2 a = DegreeToVector2(gameObject.transform.eulerAngles.z-90);
+            reversing = 180;
+            Move(a,rb, Maxspeed);
+            ButtonPressedLR();
+        }
+        else if(Input.GetKey(KeyCode.UpArrow)){
+            Vector2 a = DegreeToVector2(gameObject.transform.eulerAngles.z+90);
+            reversing = 0;
+            Move(a,rb, Maxspeed);
+            ButtonPressedLR(); 
+        }
+
+        ButtonPressedLR();
+    }
+
+    void ButtonPressedLR(){
+        //This function allows for simultaneous button presses.
         if(Input.GetKey(KeyCode.LeftArrow)){
-            Move(Vector2.left);
+            Vector2 a = DegreeToVector2(gameObject.transform.eulerAngles.z + 180 + reversing);
+            Move(a,wheel,rb.velocity.magnitude*10);
         }
 
         else if (Input.GetKey(KeyCode.RightArrow)){
-            Move(Vector2.right);
+            Vector2 a = DegreeToVector2(gameObject.transform.eulerAngles.z + reversing);
+            Move(a,wheel,rb.velocity.magnitude*10);
         }
 
-        else if (Input.GetKey(KeyCode.DownArrow)){
-            Move(Vector2.down);
-        }
-        else if(Input.GetKey(KeyCode.UpArrow)){
-            Move(Vector2.up);
-        }
     }
+
     public Vector3 CurrentPos(){
         return transform.position;
+    }
+
+    public static Vector2 RadianToVector2(float radian)
+    {
+        return new Vector2(Mathf.Cos(radian), Mathf.Sin(radian));
+    }
+      
+    public static Vector2 DegreeToVector2(float degree)
+    {
+        return RadianToVector2(degree * Mathf.Deg2Rad);
     }
 }
